c6f93e6ea38a0f49a4ff67b651e0945affaebfb3
kanzul-iman <kanzuliman2k21@gmail.com>
2025-06-28 13:31:19 -0700

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSearch() {
+      const box = document.getElementById("searchBox");
+      if (box.style.display === "inline") {
+        box.style.display = "none";
+      } else {
+        box.style.display = "inline";
+      }
+    }
